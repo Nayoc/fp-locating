8e821df50bdb1677ec080494a12dc47f659a70ec
cheng.qian365 <cheng.qian365@chinaredstar.com>
2025-12-05 11:43:00 +0800

    train wifi

--- src/train.py
+++ src/train.py
@@ -8,17 +8,18 @@ import torch.utils.data as data
 import db.mapper as mapper
 import exec.mtrain as train
 import net.mnn as mnn
-from data.optimizer import MaxminNorm
 
 root_dir = str(Path(os.path.abspath(__file__)).parent.parent)
 data_dir = root_dir + '/data'
 
 
 def run(space_id: int, dataset_id: int, data_path, batch_size=50, epochs=300, record_term=30):
+    print('current train data:' + data_path)
     label_norm = None
 
     file_path = data_dir + data_path
-    cell_train_iter, cell_test_iter, wifi_train_iter, wifi_test_iter, fusion_train_iter, fusion_test_iter = load_data(file_path, batch_size)
+    cell_train_iter, cell_test_iter, wifi_train_iter, wifi_test_iter, fusion_train_iter, fusion_test_iter = load_data(
+        file_path, batch_size)
 
     # net = mnn.CellWifiFusionModel(cell_in_channels=cell_train_iter.dataset[0][0].shape[0], wifi_in_channels=1, base_feat_channels=5,
     #                             fused_hidden=128, cell_bias_init=1.0)
@@ -36,14 +37,13 @@ def run(space_id: int, dataset_id: int, data_path, batch_size=50, epochs=300, re
         #             model_file, record_term=record_term)
 
         train.train(net, wifi_train_iter, wifi_test_iter, loss, epochs, label_norm,
-                    model_file, record_term=record_term,mode='single')
+                    model_file, record_term=record_term, mode='single')
     except Exception as e:
         mapper.update_dataset_status(dataset_id, 'fail')
         raise e
     mapper.update_dataset_status(dataset_id, 'done')
     mapper.update_dataset_model(dataset_id, model_file)
     print('——————————————————————ending training——————————————————————')
-
 
 
 def load_data(path, batch_size=64):
@@ -62,12 +62,12 @@ def load_data(path, batch_size=64):
                                      pin_memory=True)
 
     fusion_train_iter = data.DataLoader(fusion['train'], batch_size=batch_size, shuffle=True, num_workers=8,
-                                 pin_memory=True)
+                                        pin_memory=True)
     fusion_test_iter = data.DataLoader(fusion['test'], batch_size=batch_size, shuffle=True, num_workers=8,
-                                      pin_memory=True)
+                                       pin_memory=True)
 
     print(fusion['train'][0][0].shape)
-    return cell_train_iter,cell_test_iter,wifi_train_iter,wifi_test_iter,fusion_train_iter,fusion_test_iter
+    return cell_train_iter, cell_test_iter, wifi_train_iter, wifi_test_iter, fusion_train_iter, fusion_test_iter
 
 
 if __name__ == '__main__':
